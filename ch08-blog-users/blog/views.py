0fc1e61c01d2f145b398491ec32623ef05931889
IwantToBeProgrammer17 <azky.haekal35@smp.belajar.id>
2025-04-27 10:17:23 +0700

    django

---
+++ ch08-blog-users/blog/views.py
@@ -0,0 +1,25 @@
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from django.urls import reverse_lazy # new
+from .models import Post
+
+class BlogListView(ListView):
+    model = Post
+    template_name = "home.html"
+    context_object_name = 'posts' # Change the context object name to 'posts'
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = "post_detail.html"
+class BlogCreateView(CreateView): # new
+    model = Post
+    template_name = "post_new.html"
+    fields = ["title", "author", "body"]
+class BlogUpdateView(UpdateView): # new
+    model = Post
+    template_name = "post_edit.html"
+    fields = ["title", "body"]
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = "post_delete.html"
+    success_url = reverse_lazy("home")
+
